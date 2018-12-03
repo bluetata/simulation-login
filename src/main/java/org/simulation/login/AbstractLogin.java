74ebf12b203f3cc5b31b26932d32a0c4880321fc
bluetata <Sekito.Lv@gmail.com>
2018-12-03 10:50:39 +0800

    Update comments code desc. of license

--- src/main/java/org/simulation/login/AbstractLogin.java
+++ src/main/java/org/simulation/login/AbstractLogin.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 [https://github.com/bluetata] All Rights Reserved.
+ * Copyright (c) 2017-2018 Sekito Lv(bluetata) <sekito.lv@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
  * use this file except in compliance with the License. You may obtain a copy of
