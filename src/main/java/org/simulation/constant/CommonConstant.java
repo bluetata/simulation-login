3cc662b5550b756f89fee6a7834fe4ce594cb532
bluetata <Sekito.Lv@gmail.com>
2018-12-19 18:30:35 +0800

    Add common constant

---
+++ src/main/java/org/simulation/constant/CommonConstant.java
@@ -0,0 +1,24 @@
+package org.simulation.constant;
+
+/**
+ * 通用常量
+ *
+ */
+public interface CommonConstant {
+
+    // 编码
+    public static final String CHARSET = "UTF-8";
+
+    // 有效
+    public static final String STATUS_YES = "1";
+
+    // 无效
+    public static final String STATUS_NO = "0";
+
+    // 文件分隔符
+    public static final String FILE_SEPARATOR = "/";
+
+    // 每页显示数
+    public static final int PAGE_SIZE = 18;
+
+}
